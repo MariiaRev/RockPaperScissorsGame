42d31801f09665d7cee982d44527ed7748ad1a2e
MariiaRev <maria.revenko2209@gmail.com>
2021-02-25 10:46:46 +0200

    Added logging in services

--- RockPaperScissorsGame.Server/Services/UsersService.cs
+++ RockPaperScissorsGame.Server/Services/UsersService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
 using RockPaperScissorsGame.Server.Options;
 using RockPaperScissorsGame.Server.Models;
 using RockPaperScissorsGame.Server.Helpers;
@@ -15,19 +16,22 @@ namespace RockPaperScissorsGame.Server.Services
         private readonly IStorage<UserStatistics> _statistics;
         private readonly JsonDataService<StatisticsDb> _statDataService;
         private readonly JsonPathsOptions _options;
+        private readonly ILogger<UsersService> _logger;
 
         public UsersService(
             IStorage<User> users,
             IStorage<UserStatistics> statistics,
             JsonDataService<UserDb> userDataService,
             JsonDataService<StatisticsDb> statDataService,
-            IOptions<JsonPathsOptions> options)
+            IOptions<JsonPathsOptions> options,
+            ILogger<UsersService> logger)
         {
             _users = users;
             _statistics = statistics;
             _userDataService = userDataService;
             _statDataService = statDataService;
             _options = options.Value;
+            _logger = logger;
         }
 
         /// <summary>
@@ -41,22 +45,29 @@ namespace RockPaperScissorsGame.Server.Services
         /// </returns>
         public async Task<bool> SaveAsync(string login, string password)
         {
+            _logger.LogInformation("Saving a new user.");
+
             // add user to the user storage
             var userId = await _users.AddAsync(new User(login, password), null, new UserEqualityComparer());
 
             // if user was not added because already exists
             if (userId == null)
             {
+                _logger.LogInformation($"User was not added because the login {login} already exists.");
                 return false;
             }
+
+            _logger.LogInformation($"New user with login {login} was added to the storage.");
 
             // create empty statistics for the new user in memory storage
             var userStatistics = new UserStatistics((int)userId);
             await _statistics.AddAsync(userStatistics, (int)userId);
+            _logger.LogInformation($"Empty statistics for the new user with login {login} was added to the storage.");
 
             // save empty statistics for the new user to the json-file database
             var statisticsToSave = ModelsMapper.ToStatisticsDb(userStatistics);
             await _statDataService.WriteAsync($"{_options.StatisticsPath}{userId}.json", statisticsToSave);
+            _logger.LogInformation($"Empty statistics for the new user with login {login} was saved to the file {_options.StatisticsPath}{userId}json.");
 
             // get all users
             var users = await _users.GetAllAsync();
@@ -66,6 +77,7 @@ namespace RockPaperScissorsGame.Server.Services
 
             // save to file
             await _userDataService.WriteAsync(_options.UsersPath, usersToSave);
+            _logger.LogInformation($"All users were rewritten in {_options.UsersPath}.");
                 
             return true;
         }
@@ -77,12 +89,15 @@ namespace RockPaperScissorsGame.Server.Services
         /// <returns>Returns no value.</returns>
         public async void SetupStorage()
         {
+            _logger.LogInformation("Initial setup of the users storage.");
             var data = await _userDataService.ReadJsonArrayAsync(_options.UsersPath);
 
             foreach (var user in data)
             {
                 await _users.AddAsync(ModelsMapper.ToUser(user), user.UserId);
             }
+
+            _logger.LogInformation($"{await _users.CountAsync()} users were added to the users storage.");
         }
     }
 }
